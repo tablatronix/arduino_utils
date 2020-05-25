22d760a93e3236165078e45ce06f2a3df751468c
tablatronix <spam@rumental.com>
2020-05-25 00:12:22 -0500

    add reset reason refs

--- wifi_funcs.h
+++ wifi_funcs.h
@@ -3,9 +3,9 @@
 
 #include <creds.h>
 
-const char* hostname   = "esp8266REFLOW";
+// const char* hostname   = "esp8266REFLOW";
 
-void init_WiFi(int timeout){
+void init_WiFi(int timeout = 10000){
     WiFi.mode(WIFI_STA);
     #ifdef ESP8266
     WiFi.setSleepMode(WIFI_NONE_SLEEP);
@@ -14,7 +14,7 @@ void init_WiFi(int timeout){
     WiFi.setSleep(false);
     #endif
 
-    WiFi.hostname(hostname);
+    // WiFi.hostname(hostname);
     unsigned long start = millis();
     WiFi.begin(SSID,PASS);
     Serial.println("[WIFI] Connecting to wifi... [" + (String)timeout + " ms]\n");
@@ -67,6 +67,73 @@ void checkWifi(){
   }
 }
 
+// uint32_t ResetReason(void)
+// {
+  
+//     user_interface.h
+//     REASON_DEFAULT_RST      = 0,  // "Power on"                normal startup by power on
+//     REASON_WDT_RST          = 1,  // "Hardware Watchdog"       hardware watch dog reset
+//     REASON_EXCEPTION_RST    = 2,  // "Exception"               exception reset, GPIO status won’t change
+//     REASON_SOFT_WDT_RST     = 3,  // "Software Watchdog"       software watch dog reset, GPIO status won’t change
+//     REASON_SOFT_RESTART     = 4,  // "Software/System restart" software restart ,system_restart , GPIO status won’t change
+//     REASON_DEEP_SLEEP_AWAKE = 5,  // "Deep-Sleep Wake"         wake up from deep-sleep
+//     REASON_EXT_SYS_RST      = 6   // "External System"         external system reset
+  
+//   return ESP_ResetInfoReason();
+// }
+
+    // if (resetInfo.reason == REASON_DEFAULT_RST) { // normal startup by power on
+    //   strcpy_P(buff, PSTR("Power on"));
+    // } else if (resetInfo.reason == REASON_WDT_RST) { // hardware watch dog reset
+    //   strcpy_P(buff, PSTR("Hardware Watchdog"));
+    // } else if (resetInfo.reason == REASON_EXCEPTION_RST) { // exception reset, GPIO status won’t change
+    //   strcpy_P(buff, PSTR("Exception"));
+    // } else if (resetInfo.reason == REASON_SOFT_WDT_RST) { // software watch dog reset, GPIO status won’t change
+    //   strcpy_P(buff, PSTR("Software Watchdog"));
+    // } else if (resetInfo.reason == REASON_SOFT_RESTART) { // software restart ,system_restart , GPIO status won’t change
+    //   strcpy_P(buff, PSTR("Software/System restart"));
+    // } else if (resetInfo.reason == REASON_DEEP_SLEEP_AWAKE) { // wake up from deep-sleep
+    //   strcpy_P(buff, PSTR("Deep-Sleep Wake"));
+    // } else if (resetInfo.reason == REASON_EXT_SYS_RST) { // external system reset
+    //   strcpy_P(buff, PSTR("External System"));
+    // } else {
+    //   strcpy_P(buff, PSTR("Unknown"));
+
+
+// String ESP32GetResetReason(uint32_t cpu_no) {
+//   // tools\sdk\include\esp32\rom\rtc.h
+//   switch (rtc_get_reset_reason( (RESET_REASON) cpu_no)) {
+//     case POWERON_RESET          : return F("Vbat power on reset");                              // 1
+//     case SW_RESET               : return F("Software reset digital core");                      // 3
+//     case OWDT_RESET             : return F("Legacy watch dog reset digital core");              // 4
+//     case DEEPSLEEP_RESET        : return F("Deep Sleep reset digital core");                    // 5
+//     case SDIO_RESET             : return F("Reset by SLC module, reset digital core");          // 6
+//     case TG0WDT_SYS_RESET       : return F("Timer Group0 Watch dog reset digital core");        // 7
+//     case TG1WDT_SYS_RESET       : return F("Timer Group1 Watch dog reset digital core");        // 8
+//     case RTCWDT_SYS_RESET       : return F("RTC Watch dog Reset digital core");                 // 9
+//     case INTRUSION_RESET        : return F("Instrusion tested to reset CPU");                   // 10
+//     case TGWDT_CPU_RESET        : return F("Time Group reset CPU");                             // 11
+//     case SW_CPU_RESET           : return F("Software reset CPU");                               // 12
+//     case RTCWDT_CPU_RESET       : return F("RTC Watch dog Reset CPU");                          // 13
+//     case EXT_CPU_RESET          : return F("or APP CPU, reseted by PRO CPU");                   // 14
+//     case RTCWDT_BROWN_OUT_RESET : return F("Reset when the vdd voltage is not stable");         // 15
+//     case RTCWDT_RTC_RESET       : return F("RTC Watch dog reset digital core and rtc module");  // 16
+//     default                     : return F("NO_MEAN");                                          // 0
+//   }
+// }
+
+// String ESP_getResetReason(void) {
+//   return ESP32GetResetReason(0);  // CPU 0
+// }
+
+// uint32_t ESP_ResetInfoReason(void) {
+//   RESET_REASON reason = rtc_get_reset_reason(0);
+//   if (POWERON_RESET == reason) { return REASON_DEFAULT_RST; }
+//   if (SW_CPU_RESET == reason) { return REASON_SOFT_RESTART; }
+//   if (DEEPSLEEP_RESET == reason)  { return REASON_DEEP_SLEEP_AWAKE; }
+//   if (SW_RESET == reason) { return REASON_EXT_SYS_RST; }
+// }
+
 String getResetReason(){
     int reason;
     #ifdef ESP8266
@@ -77,7 +144,7 @@ String getResetReason(){
       for(int i=0;i<2;i++){
         int reason = rtc_get_reset_reason(i);
         return (String)reason;
-        // switch (reason)`
+        // switch (reason)
         // {
         // }
       }
