bbfd50beae2abc3fdc184647891592b09198a4c2
tablatronix <spam@rumental.com>
2020-06-07 17:14:26 -0500

    tuning pid

--- pid.h
+++ pid.h
@@ -33,25 +33,77 @@ bool DEBUG_pid       = false;
 //Define Variables we'll be connecting to
 double Setpoint, Input, Output;
 
+// profiling open
+// pulse 100% @ 23C
+// start  0s 23C
+// dead  20s 24C
+// 63%   46s 67C
+// end   63s 107C
+// DPV = 84
+// slope 1.3C/s from start
+// slope 1.9C/s from dead
+// Peak/OS 105s 133C (26C rise/42s) 1.6C/s coasting
+
+
+// This stealth tuning applies to dead time processes. If the process has a lag, then this tuning still applies if the lag is 1/5 or less than the size of the dead time. The stealth tuning uses the process dead time and the process gain:
+// controller gain = 0.3/(process gain)
+// integral time = .42 * (process dead time)
+
+// P = (A / PG) * (DT/TC)b
+// I = TC / (A * (DC/TC)b ??
+// D = TC * A(DT/TC)b
+
+// P 1.22
+// I 28.27
+  
+// P 1.76
+// I 31.73
+// D 13.15
+
+// 1.57
+// 37.35
+// 17.05
+
+// double Kp = 2;
+// double Ki = 0;
+// double Kd = 0;
+
+// P = 300, I = 30 and D = 0.
+
+// double Kp = 1;
+// double Ki = .01;
+// double Kd = 0;
+
+// of Kp=1:Kd=0.5:Ki=0.025.
+
+
 //Specify the links and initial tuning parameters
-double Kp=3, Ki=0, Kd=1; // P+1
+// PID HEATING Parallel independant
+// double Kp=15, Ki=0, Kd=0; // P+1
+// 0.01 for Kp and 0.0001 for Ki
+
+double Kp=3, Ki=0, Kd=.4; // GOOD FOR NOW, slight over/undershoots depending on range
 PID myPID(&Input, &Output, &Setpoint, Kp, Ki, Kd, DIRECT);
 // PID myPID(&currentTempAvg, &pidduty, &wantedTemp, Kp, Ki, Kd, DIRECT);
 
 void init_PID(){
   // float Kp=97.403, Ki=3.142, Kd=754.9, Hz=10;
   Serial.println("[PID] init");
+  Serial.println("[PID] PID : " + (String)Kp + " " + (String)Ki + " " + (String)Kd);
   myPID.SetMode(AUTOMATIC);
-  // myPid.SetOutputLimits();
+  // myPid.SetOutputLimits(low,high); // depnding on tghe ssr freq, low value do nothing < 5%
+  // myPID.SetSampleTime(120);
   // if(!res) Serial.println("[ERROR] init FAILED (outputrange)");
   // if(myPID.err()) Serial.println("[ERROR] init FAILED (construct)");
 }
 
 void run_PID(){
-  updateTemps();
-  myPID.Compute();
+  // updateTemps(); // this is too close to the PID freq
+  // make sure temps are updated before calling run pid, but do not run close to the same frequency
+  // this will cause oscillations due to noise induced inthe TC for example
   Setpoint = (double)wantedTemp;
   Input = (double)currentTempAvg;
+  myPID.Compute();
   // Serial.print("-");
   // Serial.print(Output);
   SetRelayFrequency(Output);
