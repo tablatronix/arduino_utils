d682f11722aca70c15985b0b9c747d48d1099bdb
tablatronix <spam@rumental.com>
2020-09-21 23:30:12 -0500

    NONWORKING test for ch2 adc on esp32

--- io_utils.h
+++ io_utils.h
@@ -85,4 +85,34 @@ void scanPins(){
   Serial.println("");
 }
 
+#ifdef ESP32
+// ADC2 restoring procedure
+// This is a Workaround to use ADC2 Pins on ESP32 when Wifi or Bluetooth is on.
+// (usually only ADC1 Pins are usable for analogRead() when Wifi or Bluetooth is on.)
+// -- We save the ADC2 control register before WifiBegin() or BluetoothBegin()
+// -- then restore its original value, then set a specific bit of the ADC2 control register   
+//to avoid inverted readings: we do the latter two before every analogRead() cycle.
+// -- This achieves ADC2 usability even when Wifi/Bluetooth are turned on!
+
+#include "esp32-hal-adc.h" // needed for adc pin reset
+#include "soc/sens_reg.h" // needed for manipulating ADC2 control register
+uint64_t reg_b; // Used to store ADC2 control register
+int value;
+
+void storeADC() {
+// Save ADC2 control register value : Do this before begin Wifi/Bluetooth
+reg_b = READ_PERI_REG(SENS_SAR_READ_CTRL2_REG);
+// Wifi.Begin(); // or similar wifi init function or Bluetooth begin()
+}
+
+void restoreADC() {
+// ADC2 control register restoring
+WRITE_PERI_REG(SENS_SAR_READ_CTRL2_REG, reg_b);
+//VERY IMPORTANT: DO THIS TO NOT HAVE INVERTED VALUES!
+SET_PERI_REG_MASK(SENS_SAR_READ_CTRL2_REG, SENS_SAR2_DATA_INV);
+Serial.println(analogRead(4));
+Serial.println(analogRead(33));
+}
+#endif
+
 #endif
