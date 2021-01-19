c625a69fc4b10d9b37124dcd0acdfa430b53a83a
tablatronix <spam@rumental.com>
2021-01-19 10:41:31 -0600

    reconect?

--- mqtt.h
+++ mqtt.h
@@ -7,11 +7,12 @@ WiFiClient espClient;
 
 PubSubClient client(espClient);
 
+String clientID = "";
 
 void MQTTreconnect() {
 
   // Loop until we're reconnected
-
+  // this is blocking
   while (!client.connected()) {
 
     Serial.print("Attempting MQTT connection...");
@@ -98,49 +99,77 @@ void MQTTcallback(char* topic, byte* payload, unsigned int length) {
 
 }
 
+long lastReconnectAttempt = 0;
+uint16_t mqttretry = 5000;
 
+boolean mqttReconnect() {
+  WiFi.reconnect();
+  return WiFi.status() == 'WL_CONNECTED';
+}
+
+void mqtt_checkconn(){
+  if (!client.connected()) {
+    long now = millis();
+    if (now - lastReconnectAttempt > mqttretry) {
+      lastReconnectAttempt = now;
+      // Attempt to reconnect
+      if (mqttReconnect()) {
+        lastReconnectAttempt = 0;
+        client.connect(clientID.c_str());
+      }
+    }
+  } else {
+    // Client connected
+    client.loop();
+  }  
+}
+
+void process_MQTT_nb(){
+  if (!client.connected()) {
+    mqtt_checkconn();
+  }
+  client.loop(); // will wait loop reconnect to mqtt
+}
 
 void process_MQTT(){
-
-  if (!client.connected()) {
-
-    MQTTreconnect();
-
-  }
-
-  client.loop();  
-
+  MQTTreconnect();
+  client.loop(); // will wait loop reconnect to mqtt
 }
 
-
+void init_MQTT(String clientid){
+  clientID = clientid;
+  client.setServer(mqtt_server_ip, 1883);
+  client.setCallback(MQTTcallback);
+  process_MQTT();
+}
 
 void init_MQTT(){
-
   client.setServer(mqtt_server_ip, 1883);
-
   client.setCallback(MQTTcallback);
-
   process_MQTT(); 
-
 }
-
 
 
 void MQTT_pub(String topic, String msg){
-
-    Serial.print("Publish message: ");
-
+    Serial.print("[MQTT] Publish message: ");
     Serial.print("topic: ");
-
     Serial.print(topic);
-
     Serial.print(" mesg: ");
-
     Serial.println(msg);
-
     client.publish(topic.c_str(), msg.c_str());
-
 }
 
+void MQTT_pub(String topic, String sensor, String value){
+    Serial.print("[MQTT] Publish message: ");
+    Serial.print("topic: ");
+    Serial.print(topic+"/"+clientID+"/"+sensor);
+    Serial.print("\t\tvalue: ");
+    Serial.println(value);
+    if(value == "") {
+      Serial.println("[ERROR] MQTT value is empty");
+      return;
+    }
+    client.publish((topic+"/"+clientID+"/"+sensor).c_str(), value.c_str());
+}
 
 #endif
