a2ed0ae90b95c27c78d4a9ae10dde1c142013f96
tablatronix <spam@rumental.com>
2021-02-08 17:00:28 -0600

    credential template

--- creds.h
+++ creds.h
@@ -1,8 +1,27 @@
+#ifndef creds_h
+#define creds_h
 
-String apikeya = "SOMEPRIVATEKEY";
-const char* SSID = "MYSSID";
-const char* PASS = "MYPASS";
+String apikeya     = "";
+const char* SSID_B   = "";
+const char* PASS_B   = "";
+
+const char* SSID = "";
+const char* PASS = "";
 
 const char* STASSID = SSID;
-const char* STAPSK = PASS;
+const char* STAPSK  = PASS;
 
+const char* ssid     = SSID;          // your network SSID (name)
+const char* password = PASS;
+
+// mqtt
+const char* mqtt_server_host = "";
+const char* mqtt_server_ip   = "";
+const int mqtt_server_port =  1883;
+
+// syslog
+const char* syslog_server_host = mqtt_server_host;
+const char* syslog_server_ip   = mqtt_server_ip;
+const int   syslog_server_port = 514;
+
+#endif
