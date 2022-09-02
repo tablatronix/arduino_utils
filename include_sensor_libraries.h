0ca0c3ea7650f4f4c23a39205639250bbcb24728
tablatronix <807787+tablatronix@users.noreply.github.com>
2022-09-02 16:29:14 -0500

    add lib_dep information

---
+++ include_sensor_libraries.h
@@ -0,0 +1,34 @@
+#include <Average.h>           // https:github.com/MajenkoLibraries/Average
+
+
+#include <log.h>
+#include <Syslog.h>            //https://github.com/arcao/Syslog.git
+#include <log_syslog.h>
+#include <StreamUtils.h>       // https://github.com/bblanchon/ArduinoStreamUtils.git
+
+
+#include <Genv_asset.h>
+
+#include <Wire.h>
+
+#include <OneWire.h>			// https://github.com/
+#include <DallasTemperature.h>	// https://github.com/
+#include <SoftwareSerial.h>		// https://github.com/
+#include <PMserial.h>			// https://github.com/avaldebe/PMserial
+#include <Adafruit_VEML6070.h>	// https://github.com/adafruit/Adafruit_VEML6070
+#include <Adafruit_PCF8591.h>	// https://github.com/adafruit/Adafruit_PCF8591
+#include <Adafruit_INA219.h>	// https://github.com/adafruit/Adafruit_INA219
+#include <Adafruit_MPU6050.h>	// https://github.com/adafruit/Adafruit_MPU6050
+#include <Adafruit_Sensor.h>	// https://github.com/adafruit/Adafruit_Sensor
+#include <Adafruit_APDS9960.h>	// https://github.com/adafruit/Adafruit_APDS9960
+#include <Adafruit_SHT31.h>		// https://github.com/adafruit/Adafruit_SHT31
+#include <Adafruit_BMP280.h>	// https://github.com/adafruit/Adafruit_BMP280
+#include <Adafruit_CCS811.h>	// https://github.com/adafruit/Adafruit_CCS811
+#include <Adafruit_BMP085.h.h>	// https://github.com/adafruit/Adafruit_BMP085
+#include <BH1750.h>				// https://github.com/claws/BH1750
+#include <GP2YDustSensor.h>		// https://github.com/
+#include <LM75A.h>				// https://github.com/QuentinCG/Arduino-LM75A-Temperature-Sensor-Library
+#include <HTU21D.h> 			// https://github.com/adafruit/Adafruit_HTU21DF_Library
+#include <SensirionI2CScd4x.h>	// https://github.com/
+
+// #include <hp_BH1750.h>  //inlude the library
